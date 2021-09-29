870a7ae13c48c1ace339ca8ff5472f0577c3bc6b
Jahid Hasan <jahidhasan2856@gmail.com>
2021-09-30 00:11:49 +0600

    some fix

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@volunteer.mhxeg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
